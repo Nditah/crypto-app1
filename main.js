0b41cee5fa9779e0433d0d1adf27d90142fdcc1d
Nditah <nditah@gmail.com>
2018-04-22 17:12:41 +0100

    3rd Commit

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
 const shell = require('electron').shell
+const ipc = require('electron').ipcMain
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -84,5 +85,10 @@ app.on('activate', () => {
     }
 })
 
+  ipc.on('update-notify-value', function (event, arg) {
+    win.webContents.send('targetPriceVal', arg)
+  })
+  
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 const electron = require('electron')
 const path = require('path')
+const axios = require('axios')
+const ipc = electron.ipcRenderer
 const BrowserWindow = electron.remote.BrowserWindow
+
+var price = document.querySelector('h1')
+var targetPrice = document.getElementById('targetPrice')
+var targetPriceVal;
+
+const notification = {
+    title: 'BTC Alert',
+    body: 'BTC just beat your target price!',
+    icon: path.join(__dirname, '../assets/images/bitcoin.png')
+}
+
+function getBTC() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
+        .then(res => {
+            const cryptos = res.data.BTC.USD
+            price.innerHTML = '$' + cryptos.toLocaleString('en')
+
+            // Notification:
+            let myNotification;
+            if (targetPrice.innerHTML != '' && targetPriceVal < res.data.BTC.USD) {
+                myNotification = new window.Notification(notification.title, notification)
+                console.log('BTC Alert')
+            }
+
+            myNotification.onclick = () => {
+                console.log('clicked')
+            }
+
+        })
+}
+
+getBTC();
+setInterval(getBTC, 10000);
 
 const notifyBtn = document.getElementById('notifyBtn')
 
@@ -10,10 +45,16 @@ notifyBtn.addEventListener('click', function (event) {
     let win = new BrowserWindow({
         frame: false,
         alwaysOnTop: true,
+        transparent: false,
         width: 400,
         height: 200
     })
     win.on('close', function () { win = null })
     win.loadURL(modalPath)
     win.show()
+})
+
+ipc.on('targetPriceVal', function (event, arg) {
+    targetPriceVal = Number(arg);
+    targetPrice.innerHTML = '$' + targetPriceVal.toLocaleString('en')
 })
